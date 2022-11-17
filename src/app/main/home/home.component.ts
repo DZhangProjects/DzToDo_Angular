94bd1952e3d459088c7608301014bf2219890220
David Zhang <dzhang@pistevodecision.com>
2022-11-16 23:21:54 -0500

    feat: home test

---
+++ src/app/main/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'td-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.styles.scss'],
+})
+export class HomeComponent {
+    constructor(
+    ) { }
+
+}
