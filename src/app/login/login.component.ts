7383dae52e2572d72b885647e32e1639c0c98c1d
David Zhang <dzhang@pistevodecision.com>
2022-12-02 20:00:53 -0500

    fix: increased memory

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'td-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.styles.scss'],
+})
+export class LoginComponent {
+    constructor(
+    ) { }
+
+}
